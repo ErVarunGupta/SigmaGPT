93c3c3f2af8d3724a099d87917b8b3374fed49fd
ErVarunGupta <varungupta0994@gmail.com>
2025-09-01 09:33:45 +0530

    Initial commit

---
+++ Backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { dbConnection } from "./db/dbConnection.js";
+import { GeminiConnection } from "./gemini/GeminiConnection.js";
+import { getGeminiAIResponse } from "./utils/GeminiAI.js";
+import chatRoute from './routes/chat.router.js'
+import authRoute from './routes/auth.router.js'
+
+
+dotenv.config();
+
+const app = express();
+const PORT = 8080;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api',chatRoute);
+app.use('/user',authRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+  dbConnection();
+});
+
+// app.post("/test", GeminiConnection);
+app.post('/test', getGeminiAIResponse)
